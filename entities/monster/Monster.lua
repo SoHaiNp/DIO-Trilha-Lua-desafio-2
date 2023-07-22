3b100bfd42fd5fea143b225f20612274e623389f
Luiz <l.guinycholas@gmail.com>
2023-07-22 02:35:50 -0300

    Adicionado Player e Monster

---
+++ entities/monster/Monster.lua
@@ -0,0 +1,12 @@
+local Monster = {}
+
+Monster.name = "Skeleton"
+Monster.description = "The skeleton is what's left from a past warrior who've been dead by the hands of Dracula and now are doomed to wander through the evil lord's castle."
+
+Monster.maxHealth = 15
+Monster.health = 15
+Monster.attack = 3
+Monster.defense = 5
+Monster.speed = 5
+
+return Monster
---
+++ entities/player/Player.lua
@@ -0,0 +1,12 @@
+local Player = {}
+
+Player.name = "The Belmont"
+Player.description = "A strong and fearless vampire hunter bounded to destroy the evil lord Dracula."
+
+Player.maxHealth = 30
+Player.health = 30
+Player.attack = 5
+Player.defense = 6
+Player.speed = 8
+
+return Player
